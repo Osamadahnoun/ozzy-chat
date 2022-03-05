fee26a091055b279901075534565f0234eb2fb1a
Osama Dahnoun <omdahnou@ncsu.edu>
2022-03-05 13:51:19 -0500

    intial file setup/ express server setup

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+
+const app = express();
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("API is Running");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(3000, console.log(`Server started on PORT ${PORT}`));
