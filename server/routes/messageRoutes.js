9452c936bb7ebc97193f9b7a8de8bb59dd441058
Osama Dahnoun <omdahnou@ncsu.edu>
2022-03-16 00:20:05 -0400

    created message routes and controllers

---
+++ server/routes/messageRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const {
+  sendMessage,
+  allMessages,
+} = require("../controllers/messageControllers");
+const { protect } = require("../middleware/authMiddlware");
+
+const router = express.Router();
+
+router.route("/").post(protect, sendMessage);
+router.route("/:chatId").get(protect, allMessages);
+
+module.exports = router;
